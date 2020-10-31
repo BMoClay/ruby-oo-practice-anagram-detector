68159c698129ff56407e3387275877f0120fbca7
BMoClay <benmorgancleveland@gmail.com>
2020-10-31 16:32:37 -0400

    first commit

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(word_array)
+        words = []
+        #binding.pry
+        word_array.each do |word_test|
+            if word_test.split("").sort == word.split("").sort
+                words << word_test
+            end
+        end
+        words
+    end
+
+end
